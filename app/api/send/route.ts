4622e6e2fcf194a4e127f1142243ccd84d7c7b26
nrsimonelli <nrsimonelli@gmail.com>
2024-07-19 15:57:21 -0500

    removed any

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,10 +1,11 @@
 import React from 'react'
 import { EmailTemplate } from '../../components/email-template'
 import { Resend } from 'resend'
+import { NextRequest } from 'next/server'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function POST(req: { json: () => any }) {
+export async function POST(req: Request | NextRequest) {
   const body = await req.json()
   try {
     const { data, error } = await resend.emails.send({
